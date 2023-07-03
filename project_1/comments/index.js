730f0cac482b8878cc9547248d0b11454d5a888d
fisjac <jfisher9882@gmail.com>
2023-07-03 17:16:57 -0500

    event-bus links

--- project_1/comments/index.js
+++ project_1/comments/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes} = require('crypto');
 const cors = require('cors')
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments',(req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -23,9 +24,24 @@ app.post('/posts/:id/comments',(req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(comments);
 
+
 });
+
+app.post('/events', (req,res) => {
+  console.log('recevied event:', req.body.type)
+  res.send({});
+})
 
 app.listen(4001, ()=> {
   console.log('listening on 4001');
