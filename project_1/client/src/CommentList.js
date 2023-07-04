ebc27c04dac8d82f1c3d0011efe4ce9197f953ca
fisjac <jfisher9882@gmail.com>
2023-07-04 15:36:45 -0500

    refactored for query service

--- project_1/client/src/CommentList.js
+++ project_1/client/src/CommentList.js
@@ -1,24 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
+import React from 'react';
 
-export default function CommentList({postId}) {
-
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-    setComments(res.data)
-  }
+export default function CommentList({comments}) {
 
   const renderedComments = comments.map(comment => {
     return (
       <li>{comment.content}</li>
     )
   })
-
-  useEffect(()=> {
-    fetchData()
-  }, [])
 
   return (
     <ul>
--- project_1/query/index.js
+++ project_1/query/index.js
@@ -7,12 +7,26 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/posts', async (req, res) => {
+const posts = {};
 
+app.get('/posts', async (req, res) => {
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'PostCreated') {
+    const {id, title} = data;
+    posts[id] = { id, title, comments: [] }
+  };
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
 
+    const post = posts[postId];
+    post.comments.push({id, content})
+  };
+  console.log(posts);
+  res.send({})
 });
 
 
