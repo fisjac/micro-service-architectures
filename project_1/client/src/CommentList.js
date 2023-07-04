7df7129a11516eaf4e8ca6b43b1987607ae6346f
fisjac <jfisher9882@gmail.com>
2023-07-04 17:43:58 -0500

    event syncing added

--- project_1/client/src/CommentList.js
+++ project_1/client/src/CommentList.js
@@ -3,8 +3,18 @@ import React from 'react';
 export default function CommentList({comments}) {
 
   const renderedComments = comments.map(comment => {
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content;
+    };
+    if (comment.status === 'pending') {
+      content = 'This comment is awaiting moderation';
+    };
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected'
+    };
     return (
-      <li>{comment.content}</li>
+      <li>{content}</li>
     )
   })
 
--- project_1/client/src/PostList.js
+++ project_1/client/src/PostList.js
@@ -7,8 +7,7 @@ import CommentList from './CommentList';
 export default function PostList() {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4002/posts');
-
+    const res = await axios.get('http://localhost:4002/posts').catch(err=>res.data=[]);
     setPosts(res.data);
   }
 
