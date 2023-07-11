667447cfc4c87807ce5f7e7c4176257db6e68cee
fisjac <jfisher9882@gmail.com>
2023-07-11 14:06:58 -0500

    adding remaining service urls

--- project_1/query/index.js
+++ project_1/query/index.js
@@ -52,7 +52,7 @@ app.post('/events', (req, res) => {
 
 app.listen(4002, async ()=> {
   console.log('listening on post 4002')
-  res = await axios.get('http://localhost:4005/events');
+  res = await axios.get('http://event-bus-srv:4005/events');
   for ( let event of res.data) {
     console.log('processing event:', 'event.type');
     handleEvent(event.type, event.data);
