46589a733ffe0224dbb2fa4f35b8f20001fe11f5
fisjac <jfisher9882@gmail.com>
2023-07-10 22:13:21 -0500

    connecting to kube cluster

--- project_1/posts/index.js
+++ project_1/posts/index.js
@@ -22,7 +22,7 @@ app.post('/posts', async (req,res)=> {
     id, title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv/events', {
     type: 'PostCreated',
     data: {
       id, title
