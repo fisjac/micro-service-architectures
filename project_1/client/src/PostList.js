f8dd7f7ad293f638ddefe0cdc24c0540fc92b56f
fisjac <jfisher9882@gmail.com>
2023-07-12 12:29:35 -0500

    removing get posts from posts service

--- project_1/client/src/PostList.js
+++ project_1/client/src/PostList.js
@@ -7,8 +7,10 @@ import CommentList from './CommentList';
 export default function PostList() {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
-    const res = await axios.get('http://dev.test/posts').catch(err=>console.log(err.message));
-    setPosts(res.data);
+    const res = await axios.get('http://dev.test/posts').catch(err=>err);
+    if (res.status == 200) {
+      setPosts(res.data);
+    }
   }
 
   useEffect(()=> {
