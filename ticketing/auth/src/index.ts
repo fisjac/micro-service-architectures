fe9aea775977b4be3461ba4130265e2f839a3f49
fisjac <jfisher9882@gmail.com>
2023-07-13 13:35:46 -0500

    auth routes

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,13 +3,18 @@ import {json} from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
 import { vanessaRouter } from './routes/vanessa';
+import { signupRouter } from './routes/signup';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(vanessaRouter);
-
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, ()=>{
   console.log('Listening on 3000')
