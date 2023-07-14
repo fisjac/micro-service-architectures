22b6f7e531cb666fbec71f05069f086ab07531ec
fisjac <jfisher9882@gmail.com>
2023-07-14 14:17:53 -0500

    error handling

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -7,14 +8,26 @@ import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 app.use(json());
 
+// routes
 app.use(currentUserRouter);
 app.use(vanessaRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async ()=> {
+  throw new NotFoundError();
+});
+
+
+// additional middlewares
+app.use(errorHandler);
 
 app.listen(3000, ()=>{
   console.log('Listening on 3000')
