63009fc3cac0316a1a69340fb8236b7e4ffd58f1
fisjac <jfisher9882@gmail.com>
2023-07-16 11:51:03 -0500

    adding password hashing

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
 
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -15,18 +16,24 @@ router.post('/api/users/signup', [
     .isLength({min: 4, max: 20})
     .withMessage('Password must be between 4 and 20 characters')
   ],
-  (req: Request, res: Response)=> {
+  async (req: Request, res: Response)=> {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array())
     };
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError()
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+      throw new BadRequestError('Email in use')
+    };
+    // console.log('Creating a user...');
+    const user = User.build({email, password});
+    await user.save();
 
-    res.send({});
+    res.status(201).send(user);
 
 });
 
