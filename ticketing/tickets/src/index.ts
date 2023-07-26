2c6cf37db78b26e7d73863e2b92f4e7b1262308c
fisjac <jfisher9882@gmail.com>
2023-07-26 11:47:22 -0500

    moving MONGO_URI to env variable

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -7,7 +7,10 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined')
   }
   try {
-    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets');
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI must be defined')
+    }
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb')
   } catch (err) {
     console.error(err)
