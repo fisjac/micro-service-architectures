f079001b799d52b30f9552f084e1cee8f6b78167
fisjac <jfisher9882@gmail.com>
2023-08-14 10:47:00 -0500

    adding version to tickets model and cascading changes

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@jf-ticketing/common';
 
@@ -8,15 +8,18 @@ import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-router.post('/api/tickets',
+router.post(
+  '/api/tickets',
   requireAuth,
   [
     body('title').not().isEmpty().withMessage('Title is required'),
-    body('price').isFloat({gt: 0}).withMessage('Price must be greater than zero')
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than zero'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const {title, price } = req.body;
+    const { title, price } = req.body;
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
 
@@ -24,11 +27,12 @@ router.post('/api/tickets',
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
-      userId: ticket.userId
-    })
+      userId: ticket.userId,
+      version: ticket.version,
+    });
 
     res.status(201).send(ticket);
   }
 );
 
-export {router as createTicketRouter};
+export { router as createTicketRouter };
