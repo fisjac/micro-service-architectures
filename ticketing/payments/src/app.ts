e5fd8a3afee8070f7de36d7bf079318bbdf78e6d
fisjac <jfisher9882@gmail.com>
2023-08-16 16:55:48 -0500

    adding new charge route

--- ticketing/payments/src/app.ts
+++ ticketing/payments/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { currentUser, errorHandler, NotFoundError } from '@jf-ticketing/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +17,7 @@ app.use(
 app.use(currentUser);
 
 // routes
+app.use(createChargeRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
---
+++ ticketing/payments/src/routes/new.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+
+import {
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+  NotFoundError,
+} from '@jf-ticketing/common';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.post(
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({ success: true });
+  }
+);
+
+export { router as createChargeRouter };
