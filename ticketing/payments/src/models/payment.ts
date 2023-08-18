c57ff9f67d5b57cd9df8f251ca2d0176edd229ca
fisjac <jfisher9882@gmail.com>
2023-08-18 13:44:50 -0500

    fixing typo

--- ticketing/payments/src/models/payment.ts
+++ ticketing/payments/src/models/payment.ts
@@ -1,5 +1,4 @@
-import mongoose from "mongoose"
-
+import mongoose from 'mongoose';
 
 interface PaymentAttrs {
   orderId: string;
@@ -12,31 +11,37 @@ interface PaymentDoc extends mongoose.Document {
 }
 
 interface PaymentModel extends mongoose.Model<PaymentDoc> {
-  build(attrs: PaymentAttrs): PaymentDoc
+  build(attrs: PaymentAttrs): PaymentDoc;
 }
 
-const paymentSchema = new mongoose.Schema({
-  orderId: {
-    required: true,
-    type: String,
+const paymentSchema = new mongoose.Schema(
+  {
+    orderId: {
+      required: true,
+      type: String,
+    },
+    stripeId: {
+      required: true,
+      type: String,
+    },
   },
-  stripeId: {
-    required: true,
-    type: String,
+  {
+    toJSON: {
+      transform(_doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-}, {
-  toJSON: {
-    transform(_doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-    }
-  }
-});
+);
 
-paymentSchema.statics.build = (attrs: PaymentAttrs) {
-  return new Payment(attrs)
-}
+paymentSchema.statics.build = (attrs: PaymentAttrs) => {
+  return new Payment(attrs);
+};
 
-const Payment = mongoose.model<PaymentDoc, PaymentModel>('Payment', paymentSchema);
+const Payment = mongoose.model<PaymentDoc, PaymentModel>(
+  'Payment',
+  paymentSchema
+);
 
-export {Payment}
+export { Payment };
