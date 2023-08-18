7f7d5058c0d9845c57ac6a8376ccfcaf6a08482c
fisjac <jfisher9882@gmail.com>
2023-08-18 14:40:14 -0500

    building new ticket page in client

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,30 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
-
 import buildClient from '../api/build-client';
 import Header from '../components/header';
 
-const AppComponent = ({Component, pageProps, currentUser}) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <Header currentUser={currentUser}/>
-      <Component {...pageProps}/>
+      <Header currentUser={currentUser} />
+      <div className="container">
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
-  )
+  );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-  const client = buildClient(appContext.ctx)
-  const {data} = await client.get('/api/users/currentuser');
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-  };
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      currentUser
+    );
+  }
 
   return {
     pageProps,
-    ...data
+    ...data,
   };
 };
 
